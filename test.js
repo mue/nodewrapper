93729916630ed3213354c6ba0b9908b9cbef3ce2
David Ralph <me@davidjcralph.co.uk>
2021-09-17 23:02:09 +0100

    update

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 const Mue = require('./lib/index.js');
 const mue = new Mue();
 
-mue.randomQuote().then(res => console.log(res))
+mue.randomQuote().then(res => console.log(res));
+mue.randomQuote('Français').then(res => console.log(res));
+mue.getQuoteLanguages().then(res => console.log(res));
+mue.randomImage().then(res => console.log(res));
+mue.getImageCategories().then(res => console.log(res));
